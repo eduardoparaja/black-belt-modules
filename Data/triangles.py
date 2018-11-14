c3575643fbc3f6da228c4bb40e1ef915654a8a56
eduardoparaja <44499403+eduardoparaja@users.noreply.github.com>
2018-11-14 23:09:10 +0100

    Data package with triangles.py

---
+++ Data/triangles.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 14 20:46:57 2018
+
+@author: edup_
+"""
+
+triangle_definitions = [{"base": 10, "height":2},{"base": 4, "height":3},{"base": 15, "height":12},{"base": 1, "height":8},
+{"base": 9, "height": 6}, {"base": 8, "height":6}, {"base":24, "height":12}, {"base":7, "height":43}, {"base":12, "height":15}]
+
+  
